71ecfa93b0d785db9d5aa85a3cd1708f598bde92
asg2016 <asg0182@gmail.com>
2017-10-04 17:51:37 +0500

    run uwsgi

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wsgi
+## simplest wsgi framework
--- simple_wsgi_app.py
+++ simple_wsgi_app.py
@@ -2,4 +2,5 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
+    print(environ)
     return [b'Hello world from a simple WSGI application!']
