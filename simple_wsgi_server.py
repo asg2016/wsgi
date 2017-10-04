409f38104562fb507378e0dd45a2cbd0423bd6e1
asg2016 <nefilim.star.7000@gmail.com>
2017-10-04 12:41:54 +0300

    create project

---
+++ simple_wsgi_server.py
@@ -0,0 +1,31 @@
+import socket
+
+
+def server_forever():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 9004))
+    server_socket.listen()
+    while True:
+        client_connection, client_address = server_socket.accept()
+        request = client_connection.recv(1024)
+        print(request)
+
+        client_connection.sendall(b"HTTP/1.1 200 OK\n\nHello!")
+        client_connection.close()
+
+
+def handle_request(server_socket, application):
+    client_connection, client_address = server_socket.accept()
+    raw_request = client_connection.recv(1024)
+    response = application(parse_request(raw_request), start_response)
+    client_connection.sendall(response)
+    client_connection.close()
+
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b'Hello world from a simple WSGI application!']
+
+
+if __name__ == '__main__':
+    server_forever()
