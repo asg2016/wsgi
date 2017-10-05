9cd763d5154e3a2f676bcb082777d88edc7b5f9d
asg2016 <asg0182@gmail.com>
2017-10-05 17:27:16 +0500

    controller GET, POST, PUT, DELETE modify siple uwsgi application

--- simple_wsgi_app.py
+++ simple_wsgi_app.py
@@ -1,6 +1,8 @@
 
-
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
+    print(environ['PATH_INFO'])
+    print(environ['REQUEST_URI'])
+    print(environ['REQUEST_METHOD'])
     print(environ)
     return [b'Hello world from a simple WSGI application!']
