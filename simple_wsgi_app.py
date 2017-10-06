c7daddfa6c1cf2dfb9d06f549ab9c315ddd3735d
alex <asg0182@gmail.com>
2017-10-06 14:54:07 -0400

    add base.py

--- simple_wsgi_app.py
+++ simple_wsgi_app.py
@@ -1,45 +1,27 @@
 import os
+from functools import update_wrapper
 
 
-# def run(host, port, app_name):
-#     pass
-#
-# def application_run(environ, start_response):
-#     app = UWSGIApp(environ, start_response)
-#     return app.get_response()
-#
-#
-# class Request():
-#     def __init__(self, environ):
-#         self.method = environ['REQUEST_METHOD']
-
-
-class UWSGIApp():
-    def __init__(self, environ, start_response):
-        self.environ = environ
-        self.start = start_response
-
-    def __call__(self):
-        self.start('200 OK', [('Content-Type', 'text/plain')])
-        yield [b'Hello class!!!']
-
-    def __iter__(self):
-        self.start('200 OK', [('Content-Type', 'text/plain')])
-        yield [b'Hello class!!!']
-
-    def get_method(self):
-        return self.environ['REQUEST_METHOD']
-
-    def get_response(self):
-        self.start('200 OK', [('Content-Type', 'text/plain')])
-        if self.get_method() == 'POST':
-            return [b'POST METHOD']
-        elif self.get_method() == 'GET':
-            return [b'GET METHOD']
-        return [b'NOTHING']
+def setupmethod(f):
+    def wrapper_func(self, *args, **kwargs):
+        if self.debug and self._got_first_request:
+            raise AssertionError('A setup function was called after the '
+                'first request was handled.  This usually indicates a bug '
+                'in the application where a module was not imported '
+                'and decorators or other functionality was called too late.\n'
+                'To fix this make sure to import all your view modules, '
+                'database models and everything related at a central place '
+                'before the application starts serving requests.')
+        return f(self, *args, **kwargs)
+    return update_wrapper(wrapper_func, f)
 
 
 class App(object):
+    def __init__(self, debug=True):
+        self.debug = debug
+        self._got_first_request = True
+
+
     def __call__(self, environ, start):
         start('200 OK', [('Content-Type', 'text/plain')])
         self.environ = environ
