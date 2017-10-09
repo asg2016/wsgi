3b472b565ff0af3613c1832f3b13ffa11f212855
asg2016 <asg0182@gmail.com>
2017-10-09 17:49:53 +0500

    callable class

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wsgi
 ## simplest wsgi framework
+
+for activate application
+
+> uwsgi --http :9090 --wsgi-file simple_wsgi_app.py --callable app
--- simple_wsgi_app.py
+++ simple_wsgi_app.py
@@ -1,30 +1,37 @@
 import os
 
-def run(host, port, app_name):
-    pass
 
-def application_run(environ, start_response):
-    app = UWSGIApp(environ, start_response)
-    return app.get_response()
-
-
-class Request():
-    def __init__(self, environ):
-        self.method = environ['REQUEST_METHOD']
+# def run(host, port, app_name):
+#     pass
+#
+# def application_run(environ, start_response):
+#     app = UWSGIApp(environ, start_response)
+#     return app.get_response()
+#
+#
+# class Request():
+#     def __init__(self, environ):
+#         self.method = environ['REQUEST_METHOD']
 
 
 class UWSGIApp():
     def __init__(self, environ, start_response):
         self.environ = environ
-        self.start_response = start_response
+        self.start = start_response
 
     def __call__(self):
+        self.start('200 OK', [('Content-Type', 'text/plain')])
+        yield [b'Hello class!!!']
+
+    def __iter__(self):
+        self.start('200 OK', [('Content-Type', 'text/plain')])
+        yield [b'Hello class!!!']
 
     def get_method(self):
         return self.environ['REQUEST_METHOD']
 
     def get_response(self):
-        self.start_response('200 OK', [('Content-Type', 'text/plain')])
+        self.start('200 OK', [('Content-Type', 'text/plain')])
         if self.get_method() == 'POST':
             return [b'POST METHOD']
         elif self.get_method() == 'GET':
@@ -32,5 +39,10 @@ class UWSGIApp():
         return [b'NOTHING']
 
 
-if __name__=='__main':
-    pass
+class App(object):
+    def __call__(selfself, environ, start):
+        start('200 OK', [('Content-Type', 'text/plain')])
+        return [b'Hello class!!!']
+
+
+app = App()
